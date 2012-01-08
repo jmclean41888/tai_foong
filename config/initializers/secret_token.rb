2d27e94b8c6e9f69d4ac122c33b2e477bdd62e9b
jmclean <Thoughtworks@JM6961.local>
2012-01-08 17:28:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaiFoong::Application.config.secret_token = 'f93743ef921eddaeb936f67d399d53dc8e7e25b79501a30b7c2ae7e14f774ad6bace93a969bef1bf03728fed7719b6a525b2535ece90178785d01ddc0732d523'
